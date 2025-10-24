261953149ab9d630fa14f94c3761f59d232f51e1
Jonathan Contois <jonathan.contois@gmail.com>
2025-10-24 06:45:43 +0200

    Add TrailRenderer emission control to toggle component
    TrailToggleComponent now manages the TrailRenderer's emitting property during activation and deactivation, ensuring the trail is shown or hidden appropriately.

--- Assets/Scripts/TrailToggleComponent.cs
+++ Assets/Scripts/TrailToggleComponent.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class TrailToggleComponent : BaseToggleComponent
 {
+    [SerializeField] private TrailRenderer m_TrailRenderer;
+    
     protected override void ActivateComponent()
     {
-        
+        if (m_TrailRenderer != null)
+        {
+            m_TrailRenderer.emitting = true;
+        }
     }
 
     protected override void DeactivateComponent()
     {
-        
+        if (m_TrailRenderer != null)
+        {
+            m_TrailRenderer.emitting = false;
+        }
     }
 }
